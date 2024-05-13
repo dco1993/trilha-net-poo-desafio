de2524b9b0c22b47039122da6166d2f2517622ab
Daniel Cintra de Oliveira <Daniel.Oliveira@fadami.com.br>
2024-05-13 12:32:38 -0300

    Resolução do desafio de POO da DIO!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "1234567890", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone("789123", "Modelo 2", "1234567891", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
